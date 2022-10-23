84f56aff77457fd721435c02832d4779eae43b46
mariasodawala <106393060+mariasodawala@users.noreply.github.com>
2022-10-23 19:13:24 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+// Create login Function here.
+function Login()
+{
+    player_name=document.getElementById("playername").value;
+    localStorage=setItem("playername",player_name);
+    window.location("gamepage.html")
+}
